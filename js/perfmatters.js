dbb85b9370ba1a29f4f33f3adda863db673bbd64
Jesse Hill <darksear@live.com>
2016-12-22 17:07:11 -0700

    initial upload
    initial upload

---
+++ js/perfmatters.js
@@ -0,0 +1,27 @@
+// Measuring the Critical Rendering Path with Navigation Timing
+// https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
+
+function logCRP() {
+  var t = window.performance.timing,
+    dcl = t.domContentLoadedEventStart - t.domLoading,
+    complete = t.domComplete - t.domLoading;
+  var stats = document.getElementById("crp-stats");
+  stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
+}
+
+window.addEventListener("load", function(event) {
+  logCRP();
+});
+
+var cssId = '//fonts.googleapis.com/css?family=Open+Sans:400,700';  
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'http://website.com/css/stylesheet.css';
+    link.media = 'all';
+    head.appendChild(link);
+}
